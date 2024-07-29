90a684e8a131a6a49d6c9083de82910b2b5f369d
AldoGumbi <154403121+AldoGumbi@users.noreply.github.com>
2024-07-28 19:41:03 -0600

    Update ProjectCard.tsx

--- src/components/minicomponents/ProjectCard.tsx
+++ src/components/minicomponents/ProjectCard.tsx
@@ -39,7 +39,7 @@ class ProjectCard extends Component<ProjectCardProps>{
            <div className='img-project' 
            style={{ backgroundImage: `url(${imagen})`,
                     backgroundPosition: 'center', 
-                    backgroundSize: 'contain',
+                    backgroundSize: 'cover',
                     backgroundRepeat: 'no-repeat'
                      }}>
             <p className='tittle'>{titulo}</p>
